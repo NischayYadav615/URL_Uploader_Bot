bc113c21ffbbb62d43f0e73af0559b9937056583
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-01-26 19:55:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "5905005671:AAH_pE5TL9TmZJMpLTVASUGggLv00t_U-ug") # Make a bot from https://t.me/BotFather and enter the token here
     
-    APP_ID = int(os.environ.get("API_ID", 19911978)) # Get this value from https://my.telegram.org/apps
+    APP_ID = int(os.environ.get("API_ID", "19911978")) # Get this value from https://my.telegram.org/apps
     
     API_HASH = os.environ.get("API_HASH", "e3f5848d4c384af9e6f1f52ca84c19c7") # Get this value from https://my.telegram.org/apps
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", 5826950002
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5826950002"
 
 )) # Your(owner's) telegram id
     
